7cc89b69ccd67a1a0ea9bfa646e909178129186e
cejuba <cejuba.cejuba@gmail.com>
2025-03-04 22:49:13 +0100

    House Keeping : Better definition of events (cleaner)

--- src/fr/cejuba/stardew/main/KeyHandler.java
+++ src/fr/cejuba/stardew/main/KeyHandler.java
@@ -1,6 +1,7 @@
 package fr.cejuba.stardew.main;
 
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 
 public class KeyHandler {
 
@@ -18,82 +19,15 @@ public class KeyHandler {
         scene.setOnKeyPressed(event -> {
 
             if(gamePanel.gameState == gamePanel.titleState) {
-                if (gamePanel.ui.titleScreenState == 0) {
-                    switch (event.getCode()) {
-                        case Z, UP -> {
-                            gamePanel.ui.commandNumber--;
-                            if (gamePanel.ui.commandNumber < 0) {
-                                gamePanel.ui.commandNumber = 2;
-                            }
-                        }
-                        case S, DOWN -> {
-                            gamePanel.ui.commandNumber++;
-                            if (gamePanel.ui.commandNumber > 2) {
-                                gamePanel.ui.commandNumber = 0;
-                            }
-                        }
-                        case ENTER -> {
-                            switch (gamePanel.ui.commandNumber) {
-                                case 0 -> gamePanel.ui.titleScreenState = 1;
-                                case 1 -> gamePanel.gameState = gamePanel.playState; //TBD
-                                case 2 -> System.exit(0);
-                            }
-                        }
-                    }
-                } else if (gamePanel.ui.titleScreenState == 1) {
-                    switch (event.getCode()) {
-                        case Z, UP -> {
-                            gamePanel.ui.commandNumber--;
-                            if (gamePanel.ui.commandNumber < 0) {
-                                gamePanel.ui.commandNumber = 3;
-                            }
-                        }
-                        case S, DOWN -> {
-                            gamePanel.ui.commandNumber++;
-                            if (gamePanel.ui.commandNumber > 3) {
-                                gamePanel.ui.commandNumber = 0;
-                            }
-                        }
-                        case ENTER -> {
-                            switch (gamePanel.ui.commandNumber) {
-                                case 0 -> {
-                                    System.out.println("Need to do fighter stuff");
-                                    gamePanel.gameState = gamePanel.playState;
-                                }
-                                case 1 -> {
-                                    System.out.println("Need to do thief stuff");
-                                    gamePanel.gameState = gamePanel.playState;
-                                }
-                                case 2 -> {
-                                    System.out.println("Need to do sorcerer stuff");
-                                    gamePanel.gameState = gamePanel.playState;
-                                }
-                                case 3 -> gamePanel.ui.titleScreenState = 0;
-                            }
-                        }
-                    }
-                }
-            }
-
-            else if (gamePanel.gameState == gamePanel.playState) {
-                System.out.println("Key Pressed" + event.getCode());
-                switch (event.getCode()) {
-                    case Z, UP -> upPressed = true;
-                    case S, DOWN -> downPressed = true;
-                    case Q, LEFT -> leftPressed = true;
-                    case D, RIGHT -> rightPressed = true;
-                    case T -> checkDrawTime = !checkDrawTime;
-                    case P -> gamePanel.gameState = gamePanel.pauseState;
-                    case ENTER -> enterPressed = true;
-                }
+                titleState(event);
+            } else if (gamePanel.gameState == gamePanel.playState) {
+                playState(event);
             } else if (gamePanel.gameState == gamePanel.pauseState) {
-                switch (event.getCode()) {
-                    case P -> gamePanel.gameState = gamePanel.playState;
-                }
+                pauseState(event);
             } else if (gamePanel.gameState == gamePanel.dialogueState) {
-                switch (event.getCode()) {
-                    case ENTER -> gamePanel.gameState = gamePanel.playState;
-                }
+                dialogueState(event);
+            } else if (gamePanel.gameState == gamePanel.characterState) {
+                characterState(event);
             }
         });
 
@@ -107,4 +41,94 @@ public class KeyHandler {
             }
         });
     }
+
+    private void titleState(KeyEvent event) {
+        if (gamePanel.ui.titleScreenState == 0) {
+            switch (event.getCode()) {
+                case Z, UP -> {
+                    gamePanel.ui.commandNumber--;
+                    if (gamePanel.ui.commandNumber < 0) {
+                        gamePanel.ui.commandNumber = 2;
+                    }
+                }
+                case S, DOWN -> {
+                    gamePanel.ui.commandNumber++;
+                    if (gamePanel.ui.commandNumber > 2) {
+                        gamePanel.ui.commandNumber = 0;
+                    }
+                }
+                case ENTER -> {
+                    switch (gamePanel.ui.commandNumber) {
+                        case 0 -> gamePanel.ui.titleScreenState = 1;
+                        case 1 -> gamePanel.gameState = gamePanel.playState; //TBD
+                        case 2 -> System.exit(0);
+                    }
+                }
+            }
+        } else if (gamePanel.ui.titleScreenState == 1) {
+            switch (event.getCode()) {
+                case Z, UP -> {
+                    gamePanel.ui.commandNumber--;
+                    if (gamePanel.ui.commandNumber < 0) {
+                        gamePanel.ui.commandNumber = 3;
+                    }
+                }
+                case S, DOWN -> {
+                    gamePanel.ui.commandNumber++;
+                    if (gamePanel.ui.commandNumber > 3) {
+                        gamePanel.ui.commandNumber = 0;
+                    }
+                }
+                case ENTER -> {
+                    switch (gamePanel.ui.commandNumber) {
+                        case 0 -> {
+                            System.out.println("Need to do fighter stuff");
+                            gamePanel.gameState = gamePanel.playState;
+                        }
+                        case 1 -> {
+                            System.out.println("Need to do thief stuff");
+                            gamePanel.gameState = gamePanel.playState;
+                        }
+                        case 2 -> {
+                            System.out.println("Need to do sorcerer stuff");
+                            gamePanel.gameState = gamePanel.playState;
+                        }
+                        case 3 -> gamePanel.ui.titleScreenState = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    private void playState(KeyEvent event) {
+        switch (event.getCode()) {
+            case Z, UP -> upPressed = true;
+            case S, DOWN -> downPressed = true;
+            case Q, LEFT -> leftPressed = true;
+            case D, RIGHT -> rightPressed = true;
+            case T -> checkDrawTime = !checkDrawTime;
+            case P -> gamePanel.gameState = gamePanel.pauseState;
+            case C -> gamePanel.gameState = gamePanel.characterState;
+            case ENTER -> enterPressed = true;
+        }
+    }
+
+    private void pauseState(KeyEvent event) {
+        switch (event.getCode()) {
+            case P -> gamePanel.gameState = gamePanel.playState;
+        }
+    }
+
+    private void dialogueState(KeyEvent event) {
+        switch (event.getCode()) {
+            case ENTER -> gamePanel.gameState = gamePanel.playState;
+        }
+    }
+
+    private void characterState(KeyEvent event) {
+        switch (event.getCode()) {
+            case C -> gamePanel.gameState = gamePanel.playState;
+        }
+    }
+
 }
